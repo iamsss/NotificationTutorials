f570ae47444b4bb54ba0d28bf9807fb39cc35f19
iamsss <saurav9760@gmail.com>
2020-08-25 13:27:20 +0530

    Adding Display Notification Method

--- app/src/main/java/com/example/notificationtutorials/MainActivity.java
+++ app/src/main/java/com/example/notificationtutorials/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.notificationtutorials;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CHANNEL_ID = "waplocal_id";
+    public static final String CHANNEL_NAME = "Waplocal";
+    public static final String CHANNEL_DESC ="WapLocal Desciption";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void DisplayNotification(){
+        NotificationCompat.Builder  mBuilder = new NotificationCompat.Builder(this,CHANNEL_ID)
+                                                    .setSmallIcon(R.drawable.ic_launcher_background)
+                                                    .setContentTitle("Hurray Its Notification")
+                .setContentText("Your First Notification")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat mNotMngr = NotificationManagerCompat.from(this);
+        mNotMngr.notify(1,mBuilder.build());
     }
 }

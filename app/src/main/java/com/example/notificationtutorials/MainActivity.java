fa2e1a74c5de42a53a8584d7ca4596145ea9f8f8
iamsss <saurav9760@gmail.com>
2020-08-27 01:21:40 +0530

    Getting FCM Token

--- app/src/main/java/com/example/notificationtutorials/MainActivity.java
+++ app/src/main/java/com/example/notificationtutorials/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.notificationtutorials;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -11,12 +12,20 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String CHANNEL_ID = "waplocal_id";
     public static final String CHANNEL_NAME = "Waplocal";
     public static final String CHANNEL_DESC ="WapLocal Desciption";
+    TextView textViewFCM;
 
     Button notBtn;
     @Override
@@ -33,11 +42,25 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+
+        textViewFCM = findViewById(R.id.textViewFCM);
         notBtn = findViewById(R.id.not_btn);
         notBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DisplayNotification();
+            }
+        });
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if(task.isSuccessful()){
+                    String token = task.getResult().getToken();
+                    textViewFCM.setText(token);
+                }else{
+                    Toast.makeText(MainActivity.this,"Something went wrong",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -50,5 +73,9 @@ public class MainActivity extends AppCompatActivity {
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         NotificationManagerCompat mNotMngr = NotificationManagerCompat.from(this);
         mNotMngr.notify(1,mBuilder.build());
+    }
+
+    private void getFCMToken(){
+
     }
 }

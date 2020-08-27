73c3939cb1e2fe76e8f968b2334cd8a11fb19bd5
iamsss <saurav9760@gmail.com>
2020-08-28 05:05:52 +0530

    Adding Notification Helper

--- app/src/main/java/com/example/notificationtutorials/NotificationHelper.java
+++ app/src/main/java/com/example/notificationtutorials/NotificationHelper.java
@@ -1,9 +1,16 @@
 package com.example.notificationtutorials;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class NotificationHelper {
 
@@ -15,5 +22,9 @@ public class NotificationHelper {
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         NotificationManagerCompat mNotMngr = NotificationManagerCompat.from(context);
         mNotMngr.notify(1,mBuilder.build());
+    }
+
+    public static void SubscribeToTopic(final String topic){
+
     }
 }
